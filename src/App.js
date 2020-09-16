2e9b40b10c75caa252fadb5545132d058d0ddc9e
Andrew Murray <darbyguitars@gmail.com>
2020-09-16 15:47:29 -0500

    showing oldschool vs new school way of api and app building
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// let party = []
+
+// const kos2 = async () => {
+// try {
+//     const response = await fetch(
+//     "http://198.58.117.70:8080/TestUFCAPI/ufc/winmethod/ko",
+//     {
+//         headers: {
+//         CORS: "Access-Control-Allow-Origin",
+//         },
+//         mode: "cors",
+//     }
+//     );
+//     const data = await response.json();
+//     return data;
+// } catch (error) {
+//     console.error(error);
+// }
+// };
+
+// kos2().then((response) => {
+// party = response;
+// return response;
+// })
+
+
+//     const newDivs = party.map(fight => {
+//         console.log(fight.fighter1) // ''
+//         let fightDiv =  document.createElement('div')
+//         fightDiv.textContent = fight.fighter1
+//         return fightDiv
+//     })
+
+//     document.getElementById('App').append(...newDivs)
+
+
+
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [party, setParty] = useState([]);
+
+  useEffect(() => {
+    // do api call
+    kos2().then((response) => {
+      setParty(response);
+      return response;
+    });
+  }, []);
+
+  const kos2 = async () => {
+    try {
+      const response = await fetch(
+        "http://198.58.117.70:8080/TestUFCAPI/ufc/winmethod/ko",
+        {
+          headers: {
+            CORS: "Access-Control-Allow-Origin",
+          },
+          mode: "cors",
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const newDivs = party.map(({ fighter1 }, index) => {
+    return <div key={index}>{fighter1}</div>;
+  });
+
+  return <div id="App">{newDivs}</div>;
 }
 
 export default App;
